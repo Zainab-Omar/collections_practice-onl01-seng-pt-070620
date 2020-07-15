dcadbd852349ea95f732c2f0090baa0ce258dd6d
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-15 04:16:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc (numbers)
+  numbers.sort do |a, b|
+   a <=> b
+  end
+end
+
+
+def sort_array_desc (numbers)
+  numbers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count (string)
+  string.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(element)
+end
+
+def reverse_array
+end
+
+def kesha_maker
+end
+
+def find_a
+end
+
+def sum_array
+end
+
+def add_s
+end
 
